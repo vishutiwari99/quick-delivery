5a9abc0a34f639c8a8cb69c8a60c9a1a970d3654
Vaibhav Tiwari <vishutiwari99@gmail.com>
2024-06-03 23:59:42 +0530

    Added shadcn ui theme

--- quick-delivery/src/app/page.tsx
+++ quick-delivery/src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <h1>Welcome to pizza</h1>
+      <Button>Button</Button>
     </main>
   );
 }
