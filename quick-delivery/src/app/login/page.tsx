7b39b343d6b5379e91fc0985831801a0737ce220
Vaibhav Tiwari <vishutiwari99@gmail.com>
2024-06-10 06:58:01 +0530

    login form ui done

---
+++ quick-delivery/src/app/login/page.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function LoginPage() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+      <div className="flex items-center justify-center py-12 px-8">
+        <div className="mx-auto grid w-full gap-6">
+          <div className="grid gap-2 ">
+            <h1 className="text-2xl font-extrabold">Let's get you Login !</h1>
+            <p className="text-muted-foreground">
+              Enter your information below
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            <Button variant="outline" className="w-full">
+              Login with Google
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?{" "}
+            <Link href="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- quick-delivery/src/app/page.tsx
+++ quick-delivery/src/app/page.tsx
@@ -1,10 +1,34 @@
+import LandingPageIcon from "@/icons/LandingPageIcon";
 import { Button } from "@/components/ui/button";
-
+import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to pizza</h1>
-      <Button>Button</Button>
+      <div className="w-[100%] ">
+        <div className="bg-secondary">
+          <Image
+            src="/assets/landing-img.png"
+            width={100}
+            height={100}
+            alt={"landing logo "}
+            className="w-full"
+          />
+          {/* <LandingPageIcon /> */}
+        </div>
+        <div className="mx-8 mt-8 text-center">
+          <h2 className="text-3xl font-bold ">
+            Best Package Delivery just for you
+          </h2>
+          <p className="leading-7 mt-4">
+            The king, seeing how much happier his subjects were, realized the
+            error of his ways and repealed the joke tax.
+          </p>
+        </div>
+        <Link href="/login" className="flex w-full px-8">
+          <Button className="w-full mt-8 text-base">Get Started</Button>
+        </Link>
+      </div>
     </main>
   );
 }
